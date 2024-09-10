e0dad052c8e5aee82463b3cf215148ff0c19f945
m93a <m93a.cz@gmail.com>
2024-09-10 21:15:22 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { browser } from "$app/environment";
+  import { ChartComponent as Chart, TraceList } from "libchartium";
+</script>
+
+{#if browser}
+  <Chart traces={TraceList.empty()} />
+{/if}
